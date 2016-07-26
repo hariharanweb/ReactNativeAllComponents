be8b52f78e54ce20db37028215ceee6673907045
Nagaraj <nagarajr@thoughtworks.com>
2016-07-26 15:19:00 +0100

    Separated components into individual tabs - Nagaraj

--- component/allComponents.js
+++ component/allComponents.js
@@ -15,47 +15,56 @@ import Tabs from 'react-native-tabs';
 
 class AllComponents extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {page:'second'};
+    this.state = { page: 'second' };
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Tabs selected={this.state.page} style={{backgroundColor:'white'}}
-              selectedStyle={{color:'red'}} onSelect={el=>this.setState({page:el.props.name})}>
-            <Text name="first">First</Text>
-            <Text name="second" selectedIconStyle={{borderTopWidth:2,borderTopColor:'red'}}>Second</Text>
-            <Text name="third">Third</Text>
-            <Text name="fourth" selectedStyle={{color:'green'}}>Fourth</Text>
-            <Text name="fifth">Fifth</Text>
+        <Tabs selected={this.state.page} style={{ backgroundColor: 'white' }}
+          selectedStyle={{ color: 'red' }} onSelect={el => this.setState({ page: el.props.name }) }>
+          <Text name="first">TextField</Text>
+          <Text name="second" selectedIconStyle={{ borderTopWidth: 2, borderTopColor: 'red' }}>Radio</Text>
+          <Text name="third">Date Picker</Text>
+          <Text name="fourth" selectedStyle={{ color: 'green' }}>Drop Down</Text>
+          <Text name="fifth">Check box</Text>
         </Tabs>
         { this.state.page === 'first' &&
-        <View>
-          <ScrollView style={{flex: 1, flexDirection: 'column', width:300}}>
-            <Text>Text field:</Text>
+          <View>
+            <Text>Text field: </Text>
             <Text>
-            Hello World
+              Hello World
             </Text>
-            <Text>Radio Button:</Text>
+          </View>
+        }
+        { this.state.page === 'second' &&
+          <View>
+            <Text>Radio Button: </Text>
             <MyRadio />
-            <MyDatePicker />
-            <Text>Picker:</Text>
+          </View>
+        }
+        { this.state.page === 'third' &&
+          <MyDatePicker />
+        }
+        { this.state.page === 'fourth' &&
+          <View>
+            <Text>Picker: </Text>
             <Picker
               selectedValue='key1'>
               <Picker.Item label="hello" value="key0" />
               <Picker.Item label="world" value="key1" />
             </Picker>
+          </View>
+        }
+        { this.state.page === 'fifth' &&
+          <View>
             <Text>Checkbox/Switch: </Text>
             <Switch
-              style={{marginBottom: 10}}
+              style={{ marginBottom: 10 }}
               value={true} />
-          </ScrollView>
-        </View>
-        }
-        { this.state.page !== 'first' &&
-          <Text style={{width:300}}>{this.state.page}</Text>
+          </View>
         }
       </View>
     );
@@ -64,6 +73,7 @@ class AllComponents extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: 300,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
